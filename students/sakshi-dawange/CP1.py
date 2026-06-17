2d8c4856f4338acbe6aeca14f904f5f7677ab4f0
dawangesakshi94-hue <dawangesakshi94@gmail.com>
2026-06-17 10:28:16 +0530

    Update CP1.py

--- students/sakshi-dawange/CP1.py
+++ students/sakshi-dawange/CP1.py
@@ -1,8 +1,10 @@
+#problem 1
 pass_mark=35
 distinction_mark=75
 def average_of_three(m1,m2,m3):
     avg=(m1+m2+m3)/3
     return round(avg,1)
+#problem 2
 def classify_result(avg):
     if avg<pass_mark:
         return "fail"
@@ -10,6 +12,7 @@ def classify_result(avg):
         return "distinction"
     else:
         return"pass"
+#problem 3
 def letter_grade(avg):
     if avg>=90:
         return"A"
@@ -21,6 +24,7 @@ def letter_grade(avg):
         return"D"
     else:
         return"F"
+#problem 4
 def format_marks(m1,m2,m3):
     return f"{m1},{m2},{m3}"
 print(average_of_three(85,90,78))
